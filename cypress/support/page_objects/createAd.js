71c929ec76edfcf6814e368c6c466b358eb778fc
taimuraamer <taimur.aamer@gmail.com>
2022-03-15 16:31:47 +0500

    main

--- cypress/support/page_objects/createAd.js
+++ cypress/support/page_objects/createAd.js
@@ -3,8 +3,26 @@ class CreateAd{
     createAd() {
         cy.contains(/menu/i).click()
         cy.get('ul li a').contains(/.+edit.+my.+ad.+/i).click({force: true})
+        cy.get('div ul li').should('have.class', 'active')
+
+        //No. of stairs
+        cy.get('#NoOfStairs').select('4 fl').should('have.value', '4')
+        cy.get('.wizard-next').click()
+
+        //Move in date
+        cy.get('#txtFromDate').click()
+        cy.get('.ui-state-default').contains('18').click()
+        cy.get('.ui-state-default').should('contain', '18')
+
+        cy.get('.MB20').click({force: true})
+        cy.get('#txtToDate').click()
+        cy.get('[title="Next"]').click()
+        cy.get('.ui-state-default').contains('16').click()
+        cy.get('.ui-state-default').should('contain', '16')
+
 
     }
+
 }
 
 export default CreateAd;
