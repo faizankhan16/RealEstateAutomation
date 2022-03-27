82c79cac1ca0ede0a50f013315338a095910a587
Faizan Khan <faizankhanworkbox@gmail.com>
2022-03-27 22:33:07 +0500

    main

--- cypress/integration/bookAView.js
+++ cypress/integration/bookAView.js
@@ -1,6 +1,8 @@
 import SignIn from "../support/page_objects/signIn";
 import credential from "../fixtures/credential.json";
+import selectors from "../fixtures/selectors.json";
 const { emailTenant, newPassword, invalidEmail, invalidPassword, oldPassword } = credential.tenantLogin;
+const { viewBooking } = selectors;
 const login = new SignIn()
 
 describe('Tests to book a view', () => {
@@ -11,10 +13,19 @@ describe('Tests to book a view', () => {
 
     it('Verify that the tenant can a book a view', () => {
         login.signIn(emailTenant, newPassword)
-        // if( cy.contains('Check your creditscore and get information about new objects 24 h before everybody else')){
-        // cy.get('div .featherlight-content span[class="icon icon-cross-circle"]').click()}
-        cy.get('img[alt="Landing image"]').click()
-        cy.get('#hero-search').type('Stockholm').type('{Enter}')
-        cy.get('[class="font-weight-normal1 ellipsis-wrap"]').contains('111 47 Stockholm').eq(2).click()
+        if(cy.contains('Check your creditscore and get information about new objects 24 h before everybody else').should('be.visible') == true){
+        cy.get('div .featherlight-content span[class="icon icon-cross-circle"]').click()}
+        cy.get(viewBooking.homePage).click()
+        cy.get(viewBooking.searchButtonLandingPage).type('Stockholm').type('{Enter}')
+        cy.get(viewBooking.selectAHome).contains('111 47 Stockholm').eq(0).scrollIntoView().click()
+        cy.window().then((win) => {
+            cy.stub(win, 'open', url => {
+                win.location.href = 'https://stage02.dev.samtrygg.se/object/lvavnrjvu5cnjusrydqo/stockholm/stockholm/hamngatan-27/4-rok';
+            }).as("popup")
+        })
+        cy.get('@popup')
+            .should("be.called")
+        cy.get(viewBooking.crossButton).click()
+        cy.get(viewBooking.moveInDate)
     })
 })
